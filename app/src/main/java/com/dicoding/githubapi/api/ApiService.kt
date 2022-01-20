bc6474781fdefcde0a1ae1714411e57545aa5db1
RIZA ZAMZAMI <63160135+mindflayers10@users.noreply.github.com>
2022-01-21 00:44:15 +0700

    Update ApiService.kt

--- app/src/main/java/com/dicoding/githubapi/api/ApiService.kt
+++ app/src/main/java/com/dicoding/githubapi/api/ApiService.kt
@@ -11,25 +11,25 @@ import retrofit2.http.Query
 interface ApiService {
 
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getUsers(
         @Query("q") query: String
     ): Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getDetail(
         @Path("username") username: String
     ): Call<User>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollower(
         @Path("username") username: String
     ): Call<ArrayList<User>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollowing(
         @Path("username") username: String
     ): Call<ArrayList<User>>
